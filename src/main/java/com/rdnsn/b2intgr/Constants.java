819d6b7f66211df70adef0d49a6b3c73cf790624
archimedon <ronniedz@icloud.com>
2018-01-27 02:32:32 -0500

    Minor reformat.

--- src/main/java/com/rdnsn/b2intgr/Constants.java
+++ src/main/java/com/rdnsn/b2intgr/Constants.java
@@ -4,6 +4,8 @@ import com.google.common.net.HttpHeaders;
 import java.nio.charset.StandardCharsets;
 
 public interface Constants {
+	public String UTF_8 = StandardCharsets.UTF_8.name();
+	public String AUTHORIZATION = HttpHeaders.AUTHORIZATION;
 
 	public String USER_FILE = "userFile";
 	public String AUTH_RESPONSE = "authResponse";
@@ -11,12 +13,8 @@ public interface Constants {
 	public String X_BZ_FILE_NAME = "X-Bz-File-Name";
 	public String X_BZ_INFO_AUTHOR = "X-Bz-Info-Author";
 	public String X_BZ_CONTENT_SHA1 = "X-Bz-Content-Sha1";
+
 	public String DOWNLOAD_URL = "downloadUrl";
 	public String TRNSNT_FILE_DESTDIR = "destDir";
-	public String UTF_8 = StandardCharsets.UTF_8.name();
 	
-	public String AUTHORIZATION = HttpHeaders.AUTHORIZATION;
-	
-	
-
 }
